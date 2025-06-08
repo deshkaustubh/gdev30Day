340f8b91abe9ff3d7fc5eaf448cdfe8fe74d87c6
Kaustubh <ksd.tcet@gmail.com>
2025-06-09 05:26:36 +0530

    optimized imports

--- app/src/main/java/com/example/a30day/30DayScreen.kt
+++ app/src/main/java/com/example/a30day/30DayScreen.kt
@@ -5,9 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -24,18 +22,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,7 +38,6 @@ import androidx.compose.ui.unit.sp
 import com.example.a30day.data.Day
 import com.example.a30day.data.Week
 import com.example.a30day.data.studyPlan
-import com.example.a30day.ui.theme._30DayTheme
 
 
 fun openLink(context: Context, url: String) {
